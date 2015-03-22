f159d46487ed75001c50c18ed4aec15b3f01b3cd
Prat <banqfirn@gmail.com>
2015-03-22 12:46:44 -0400

    Fix menu

--- app/src/main/java/com/prt2121/amu/ui/LocationActivity.java
+++ app/src/main/java/com/prt2121/amu/ui/LocationActivity.java
@@ -62,28 +62,6 @@ public class LocationActivity extends ActionBarActivity {
                 .commit();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_location, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     public void setActionBarTitle(String title){
         setTitle(title);
     }
